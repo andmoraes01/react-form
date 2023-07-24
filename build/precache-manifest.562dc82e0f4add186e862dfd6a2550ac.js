a99d1103b5c3b2aa44f8661136143c3b5527763f
André Moraes <andre.moraes@dtidigital.com.br>
2023-07-24 16:38:50 -0300

    feat: adjustments after run build
    work item: 0000

---
+++ build/precache-manifest.562dc82e0f4add186e862dfd6a2550ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae668190602a501d4c9aac4ebc965bb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8c54efb5463a54e7dc5",
+    "url": "/static/css/main.68020d32.chunk.css"
+  },
+  {
+    "revision": "ee5007e3873c85d7a5ef",
+    "url": "/static/js/2.c11e3392.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c11e3392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c54efb5463a54e7dc5",
+    "url": "/static/js/main.9eb9caf7.chunk.js"
+  },
+  {
+    "revision": "0340490fbc318832aebf",
+    "url": "/static/js/runtime-main.1d2ea878.js"
+  }
+]);
