677a4b994482fb3a1e146dda744912f610be5eba
André Moraes <andre.moraes@dtidigital.com.br>
2023-07-24 09:57:05 -0300

    feat: installing formik and creating form
    work item: 0001

--- src/components/AddCostumer.jsx
+++ src/components/AddCostumer.jsx
@@ -1,25 +1,58 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 const AddCostumer = () => {
   return (
     <>
       <h1>Cadastro de Clientes</h1>
 
-      <form>
-        <div className="form-group">
-          <label htmlFor="name">Nome</label>
-          <input id="name" name="name" type="text" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input id="email" name="email" type="email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="date">Data de Nascimento</label>
-          <input id="birth" name="birth" type="date" />
-        </div>
-        <button type="submit">Adicionar</button>
-      </form>
+      <Formik
+        initialValues={
+          { name: '', email: '@mail.com', birth: '' }
+        }
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {(props) => (
+          <form
+            onSubmit={props.handleSubmit}
+            noValidade
+          >
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                name="name"
+                type="text"
+                value={props.values.name}
+                onChange={props.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                name="email"
+                type="email"
+                value={props.values.email}
+                onChange={props.handleChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="date">Data de Nascimento</label>
+              <input
+                id="birth"
+                name="birth"
+                type="date"
+                value={props.values.birth}
+                onChange={props.handleChange}
+              />
+            </div>
+            <button type="submit">Adicionar</button>
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
